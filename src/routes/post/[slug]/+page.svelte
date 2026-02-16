aa430a916e5e5b915b4ad013ea8f736761e4a0fb
Colin Reddig <crreddig@gmail.com>
2026-02-16 11:11:34 -0500

    First post

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -4,9 +4,23 @@
   // You can do things with data.metadata here
 </script>
 
-<article class="p-8 prose dark:prose-invert
-  prose-h1:font-bold prose-h1:text-xl
-  prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-xl
-  prose-headings:underline">
-    <svelte:component this={data.content} />
-</article>
+<section class="text-gray-600 body-font overflow-hidden">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="-my-8 divide-y-2 divide-gray-100">
+      <div class="py-8 flex flex-wrap md:flex-nowrap">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span class="font-semibold title-font text-gray-700">{data.metadata.category}</span>
+          <span class="mt-1 text-gray-500 text-sm">{data.metadata.date}</span>
+        </div>
+        <div class="md:flex-grow">
+          <p class="leading-relaxed prose dark:prose-invert
+          prose-h1:font-bold prose-h1:text-xxl
+          prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-xl
+          prose-headings:bold"> 
+          <svelte:component this={data.content} />
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
